c4516a4c8144e615e486b009c4c3c0bde74004c9
Sagar Bhatt <Sagarbhatt772@gmail.com>
2022-05-27 14:05:39 +0530

    Implemented DRF using function based view
    * create the object using POST request

--- drf_app/urls.py
+++ drf_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.listview, name='alldata'),
     path('<int:pk>', views.detailview, name='oneobject'),
+    path('create/', views.createview, name='create'),
 
 ]
--- drf_app/views.py
+++ drf_app/views.py
@@ -1,7 +1,9 @@
 from .serializers import StudentSerializer
 from .models import Student
 from django.http import JsonResponse
-# Create your views here.
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 def listview(request):
@@ -19,3 +21,20 @@ def detailview(request,pk):
     stu = Student.objects.get(id=pk)
     serializer = StudentSerializer(stu)
     return JsonResponse(serializer.data)
+
+@csrf_exempt
+def createview(request):
+    """
+    to add the create the object
+    """
+    if request.method == "POST":
+        stu_data = JSONParser().parse(request)
+        serializer = StudentSerializer(data = stu_data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        else:
+            return JsonResponse(serializer.errors)
+    else:
+        resp = {'msg':'its a get request'}
+        return JsonResponse(resp)
