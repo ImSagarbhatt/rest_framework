49be2f4bfbc1aa94b11c7735114cd9f8442fb81c
Sagar Bhatt <Sagarbhatt772@gmail.com>
2022-05-28 16:31:05 +0530

    Fixed some issues

--- drf_app/views.py
+++ drf_app/views.py
@@ -3,50 +3,54 @@ from .models import Student
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-
+@api_view()
 def listview(request):
     """
     to view the all the serializer data
     """
     stu = Student.objects.all()
     serializer = StudentSerializer(stu, many=True)
-    return JsonResponse(serializer.data,safe=False)
+    return Response(serializer.data)
 
+@api_view()
 def detailview(request,pk):
     '''
     to get the one object
     '''
     stu = Student.objects.get(id=pk)
     serializer = StudentSerializer(stu)
-    return JsonResponse(serializer.data)
+    return Response(serializer.data)
 
 @csrf_exempt
+@api_view(["GET","POST"])
 def createview(request):
     """
     to add the create the object
     """
     if request.method == "POST":
-        stu_data = JSONParser().parse(request)
+        stu_data= request.data
         serializer = StudentSerializer(data = stu_data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors)
+            return Response(serializer.errors)
     else:
-        resp = {'msg':'its a get request'}
-        return JsonResponse(resp)
+        return Response(serializer.data)
 
 @csrf_exempt
+@api_view(["PUT"])
 def updateview(request):
     """
     to update the value of object
     """
     if request.method == "PUT":
         try:
-            stu_data = JSONParser().parse(request)
+            stu_data = request.data
             id = stu_data.get("id")
             stu = Student.objects.get(id=id)
             serializer = StudentSerializer(stu,data=stu_data)
@@ -60,20 +64,21 @@ def updateview(request):
 
 
 @csrf_exempt
+@api_view(["DELETE"])
 def deleteview(request):
     """
     delete the object
     """
     if request.method =="DELETE":
         try:
-            stu_data = JSONParser().parse(request)
+            stu_data = request.data
             id = stu_data.get("id")
             stu = Student.objects.get(id=id)
             stu.delete()
-            return JsonResponse({"msg":"Deleted successfully"})
+            return Response({"msg":"Deleted successfully"})
         except Exception:
-            return JsonResponse({"msg":"Object not found!!"})
+            return Response({"msg":"Object not found!!"})
     else:
-        return JsonResponse({"msg":" Its a GET request!!"})
+        return Response({"msg":" Its a GET request!!"})
 
 
